e9e4c133bc3231f2c2ceaec8ed082401ab2f327e
Avinash Madduru <avinashmadduru26@gmail.com>
2023-12-17 22:44:20 +0530

    first commit

---
+++ routes/workouts.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const Workout = require("../models/workoutModel");
+
+const {
+  getWorkouts,
+  getWorkout,
+  createWorkout,
+  deleteWorkout,
+  updateWorkout,
+} = require("../controller/workoutController");
+
+const router = express.Router();
+
+router.get("/", getWorkouts);
+
+router.get("/:id", getWorkout);
+
+router.post("/", createWorkout);
+
+router.delete("/:id", deleteWorkout);
+
+router.patch("/:id", updateWorkout);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts");
+
+const app = express();
+
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.use("/api/workouts", workoutRoutes);
+
+mongoose
+  .connect(process.env.MONG_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log("connected to DB & listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
